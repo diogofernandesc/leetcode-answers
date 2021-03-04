eaa95cac66f37b0bf8548be13db090a9d5c41533
diogofernandesc <diogofernandescon@gmail.com>
2021-03-04 20:07:25 +0000

    better solutio

--- squares_of_sorted_array/squares_of_sorted_array.java
+++ squares_of_sorted_array/squares_of_sorted_array.java
@@ -1,12 +1,20 @@
-import java.util.Arrays;
 class Solution {
     public int[] sortedSquares(int[] A) {
-        for (int i = 0; i < A.length; i++) {
-            A[i] = A[i] * A[i];
+        int n = A.length;
+        int[] newArr = new int[n];
+        int left = 0;
+        int right = A.length - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (Math.abs(A[left]) > Math.abs(A[right])) {
+                newArr[i] = A[left] * A[left];
+                left++;
+
+            } else {
+                newArr[i] = A[right] * A[right];
+                right--;
+            }
         }
-        Arrays.sort(A);
-
-        return A;
-
+        return newArr;
     }
 }
--- squares_of_sorted_array/squares_of_sorted_array.py
+++ squares_of_sorted_array/squares_of_sorted_array.py
@@ -1,4 +1,16 @@
 class Solution:
     def sortedSquares(self, A: List[int]) -> List[int]:
-        A = [x**2 for x in A]
-        return sorted(A)
+        new_arr = [x for x in range(0, len(A))]
+        n = len(A)
+        left = 0
+        right = len(A) - 1
+
+        for i in range(n - 1, -1, -1):
+            if abs(A[left]) > abs(A[right]):
+                new_arr[i] = A[left] ** 2
+                left += 1
+            else:
+                new_arr[i] = A[right] ** 2
+                right -= 1
+
+        return new_arr
