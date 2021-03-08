5b25f40323bb760b8d5cab6880ad2ef7c307f251
diogofernandesc <diogofernandescon@gmail.com>
2021-03-08 20:03:15 +0000

    Better solution for n and double + leetcode solution

--- check_if_n_and_its_double_exist/check_if_n_and_its_double_exist.java
+++ check_if_n_and_its_double_exist/check_if_n_and_its_double_exist.java
@@ -17,3 +17,49 @@ class Solution {
         
     }
 }
+
+// Solution o(n) with HashMap
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        int foundIndex = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i] * 2)) {
+                foundIndex = map.get(arr[i] * 2);
+
+            } else if (map.containsKey(arr[i])) {
+                foundIndex = map.get(arr[i]);
+
+            } else if (arr[i] % 2 == 0 && map.containsKey(arr[i] / 2)) {
+                foundIndex = map.get(arr[i] / 2);
+
+            } else {
+                map.put(arr[i], i);
+            }
+
+            if (foundIndex != -1 && foundIndex != i) {
+                // Also handling edge case of [0,0]
+                if (arr[foundIndex] != arr[i] || arr[i] == 0 && arr[foundIndex] == 0) {
+                    return true;
+                }
+                foundIndex = -1;
+            }
+
+
+        }
+        return false;
+
+
+
+    }
+}
+// Best leetcode solution:
+ public boolean checkIfExist(int[] arr) {
+    Set<Integer> seen = new HashSet<>();
+    for (int i : arr) {
+        if (seen.contains(2 * i) || i % 2 == 0 && seen.contains(i / 2))
+            return true;
+        seen.add(i);
+    }
+    return false;
+ }
