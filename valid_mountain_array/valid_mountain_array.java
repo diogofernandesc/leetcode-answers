c1fce75a8aab760078f0682a107cad469d280edf
diogofernandesc <diogofernandescon@gmail.com>
2021-04-11 19:54:09 +0100

    another go at mountain array

--- valid_mountain_array/valid_mountain_array.java
+++ valid_mountain_array/valid_mountain_array.java
@@ -31,3 +31,64 @@ class Solution {
         
     }
 }
+
+class Solution2 {
+    public boolean validMountainArray(int[] arr) {
+        Integer peakIndex = null;
+        boolean climbed = false;
+        boolean descended = false;
+        if (arr.length < 3) {
+            return false;
+        }
+
+        for (int i = 0 ; i < arr.length; i++) {
+            if (i + 1 < arr.length) {
+                if (arr[i] < arr[i+1]) {
+                    climbed = true;
+                }
+                if (peakIndex != null) {
+                    return false;
+                }
+            } else {
+                descended = true;
+                if (peakIndex == null ) {
+                    peakIndex = i;
+                }
+                if (peakIndex != null && i < peakIndex) {
+                    return false;
+                }
+                if (peakIndex != null && arr[i] == arr[i+1]) {
+                    return false;
+                }
+            }
+        }
+        if (peakIndex == null || !climbed || !descended) {
+            return false;
+        }
+        return true;
+    }
+}
+
+class OptimalSolution {
+    // from leetcode
+    public boolean validMountainArray(int[] A) {
+            int N = A.length;
+            int i = 0;
+
+            // walk up
+            while (i+1 < N && A[i] < A[i+1])
+                i++;
+
+            // peak can't be first or last
+            if (i == 0 || i == N-1)
+                return false;
+
+            // walk down
+            while (i+1 < N && A[i] > A[i+1])
+                i++;
+
+            return i == N-1;
+        }
+    
+}
+
