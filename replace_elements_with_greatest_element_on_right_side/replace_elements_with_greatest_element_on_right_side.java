0c5ee22890e10f657c6823888b848aa3c2ad6cc5
diogofernandesc <diogofernandescon@gmail.com>
2021-05-05 20:35:12 +0100

    prettier solutions

--- replace_elements_with_greatest_element_on_right_side/replace_elements_with_greatest_element_on_right_side.java
+++ replace_elements_with_greatest_element_on_right_side/replace_elements_with_greatest_element_on_right_side.java
@@ -1,22 +1,37 @@
+// Prettier O(n) solution
 class Solution {
     public int[] replaceElements(int[] arr) {
         int highestVal = arr[arr.length-1];
         for (int i = arr.length - 2; i >= 0; i--) {
-            if (arr[i+1] > highestVal) {
-                highestVal = arr[i+1];
-            }
-            if (arr[i] > highestVal) {
-                // Before overwriting the value based on previous highest, set first
-                int tempVal = arr[i];
-                arr[i] = highestVal;
-                highestVal = tempVal;
-            
-            } else {
-                arr[i] = highestVal;
+            int tmpVal = arr[i];
+            arr[i] = highestVal;
+            if (tmpVal > highestVal) {
+                highestVal = tmpVal;
             }
         }
         arr[arr.length-1] = -1;
         return arr;
     }
-    
+}
+
+// O(n)2 solution with check from left
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int highestVal;
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length - 1) {
+                highestVal = arr[i+1];
+                for (int j = i + 2; j < arr.length; j++) {
+                    if (arr[j] > highestVal) {
+                        highestVal = arr[j];
+                    }
+                }
+                arr[i] = highestVal;
+            
+            } else {
+                arr[i] = -1;
+            }
+        }
+        return arr;
+    }
 }
--- replace_elements_with_greatest_element_on_right_side/replace_elements_with_greatest_element_on_right_side.py
+++ replace_elements_with_greatest_element_on_right_side/replace_elements_with_greatest_element_on_right_side.py
@@ -2,16 +2,10 @@ class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
         highest_val = arr[-1];
         for i in range(len(arr)-2, -1, -1):
-            if arr[i+1] > highest_val:
-                highest_val = arr[i+1]
+            tmp_val = arr[i]
+            arr[i] = highest_val
+            if (tmp_val > highest_val):
+                highest_val = tmp_val
             
-            if arr[i] > highest_val:
-                temp_val = arr[i]
-                arr[i] = highest_val
-                highest_val = temp_val
-        
-            else:
-                arr[i] = highest_val
-        
         arr[-1] = -1
         return arr
