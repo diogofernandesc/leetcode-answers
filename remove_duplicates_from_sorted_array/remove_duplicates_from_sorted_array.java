2745146b8eddc868d4d1e6be95fe98e6acb39505
diogofernandesc <diogofernandescon@gmail.com>
2021-05-08 17:36:54 +0100

    another solution to remove duplicaes from sorted array

--- remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.java
+++ remove_duplicates_from_sorted_array/remove_duplicates_from_sorted_array.java
@@ -13,4 +13,18 @@ class Solution {
     }
 }
 
-// Use the pointer again, this time just need to keep track of the last seen element (i - 1) as the value to compare against.
+// Alternatively, where wp is a writer pointer and i is read pointer
+ 
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int wp = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (i + 1 < nums.length) {
+                nums[wp] = nums[i+1];
+                wp++;
+            }
+        }
+        return wp;
+    }
+}
+
